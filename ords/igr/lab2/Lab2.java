faca2b16129bc5163942eb613f3915a7200782ea
Artem Miakota <artem-miakota@users.noreply.github.com>
2017-04-13 19:47:33 -0400

    make Igor variant Lab2

--- ords/igr/lab2/Lab2.java
+++ ords/igr/lab2/Lab2.java
@@ -1,15 +1,15 @@
-public class Lab2 {
-    private static final double P0 = 1.29;
-    private static final double z = 0.000125;
+package lab2;
 
-     public static void main(String []args) {
-        double P;
-        for (int h = 0; h < 10000; h++) {
-            P = P0 * Math.pow(Math.E, -1 * h * z);
-            if (P < 1) {
-                System.out.print("h = " + h + "  P = " + P + "\n");
-                return;
-            }
+public class Lab2 {
+    public static void main(String[] args) {
+        double S = 0;  // загальний шлях
+        double N = 10; // дення норма
+        int D = 10;    //кількість днів
+        
+        for (int i = 0; i < D; i++) {
+            S += N;
+            N *= 1.1;  // збільшуємо на щоденний шлях на 10%
         }
-     }
+        System.out.print("Спортсмен пробіжить " + S + " км за " + D + " днів\n");
+    }
 }
