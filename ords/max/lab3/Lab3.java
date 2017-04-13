0bd428c63d42bda8a1c0974594d39273a3bec89f
user <user@PC.(none)>
2017-04-12 22:36:00 -0400

    uncomment lines

--- ords/max/lab3/Lab3.java
+++ ords/max/lab3/Lab3.java
@@ -2,10 +2,10 @@ package lab3;
 
 public class Lab3 {
     public static void main(String[] args) {
-//        Student s = Student.dataEnter();
-//        System.out.println("Вік студента: " + s.getAge()
-//                + "\nСередній бал студента: " + s.getAverageMark()
-//                + "\nРозмір стипендії студента: " + s.getRevenue());
+        Student s = Student.dataEnter();
+        System.out.println("Вік студента: " + s.getAge()
+                + "\nСередній бал студента: " + s.getAverageMark()
+                + "\nРозмір стипендії студента: " + s.getRevenue());
 
         Dealer d = Dealer.dataEnter();
         d.addCar();
